8a86cc0cadd3487b4a13fe1e9b88a25b7cba2d2f
Vlad Malcev <vlad_malcev2004@mail.ru>
2024-02-13 17:49:56 +0700

    Connection to PostgreSQL in Docker

--- src/main/java/ru/kors/springstudents/model/Student.java
+++ src/main/java/ru/kors/springstudents/model/Student.java
@@ -1,15 +1,31 @@
 package ru.kors.springstudents.model;
 
-import lombok.*;
+import jakarta.persistence.*;
+        import lombok.*;
 
-import java.time.LocalDate;
+        import java.time.LocalDate;
+import java.time.Period;
 
 @Data
-@Builder
+@Entity
+@Table(name = "students")
 public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+    //    Поменять имя в колнки
+    @Column(name = "firstName")
     private String firstName;
     private String lastname;
     private LocalDate dataOfBirth;
+    // уникальное поле среди полщователей
+    @Column(unique = true)
     private String email;
+    //    Возраст генерируется на основе даты рождения
+    @Transient
     private int age;
+
+    public int getAge(){
+        return Period.between(dataOfBirth, LocalDate.now()).getYears();
+    }
 }
--- src/main/java/ru/kors/springstudents/model/University.java
+++
@@ -1,7 +0,0 @@
-package ru.kors.springstudents.model;
-
-import java.util.List;
-
-public class University {
-    List<Student> students;
-}
